9bef24d74ad31b0471d084f3d1fb539b677bf4d4
Krishy <krishy.b@gmail.com>
2024-12-23 12:55:05 +0530

    Update to original

--- CommandLineInterface.py
+++ CommandLineInterface.py
@@ -1,5 +1,10 @@
+from mailbox import mboxMessage
+from os import write
+
 import functions
 import time
+
+from functions import get_todos, write_todos
 
 now = time.strftime("%b %d, %Y %H:%M:%S")
 print("It is", now)
@@ -12,4 +17,59 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]
 
+        todos = get_todos()
 
+        todos.append(todo +'\n')
+
+        write_todos(todos)
+
+    elif user_action.startswith("show"):
+
+        todos = get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            row = f"{index+1}-{item}"
+            print(row)
+
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            print(number)
+
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+
+            write_todos(todos)
+        except ValueError:
+            print("Your command is not valid")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos()
+            index = number-1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
+        except IndexError:
+            print("There is no ite with that number.")
+            continue
+
+    elif user_action.startswith("exit"):
+        break
+
+    else:
+        print("Command is not valid.")
+
+print("Bye!")
